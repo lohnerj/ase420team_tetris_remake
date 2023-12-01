8dbaa6a40d2e9e29d098e32df26b812f3462e4a1
lohnerj <lohnerj1@mymail.nku.edu>
2023-12-01 14:15:55 -0500

    Comments

--- integration1.py
+++ integration1.py
@@ -12,7 +12,7 @@ class TestColorIntegration(unittest.TestCase):
         
 
     
-
+    #Regression
     def test_color_methods(self):
         self.assertEqual(self.color.get_purple(), (120, 37, 179))
         self.assertEqual(self.color.get_light_blue(), (100, 179, 179))
@@ -21,7 +21,7 @@ class TestColorIntegration(unittest.TestCase):
         self.assertEqual(self.color.get_red(), (180, 34, 22))
         self.assertEqual(self.color.get_pinkish_purple(), (180, 34, 122))
         self.assertEqual(self.color.get_orange(), (255, 97, 3))
-        
+    #Regression
     def test_starting_values_methods(self):
         self.assertEqual(self.starting_values.get_startX(), 100)
         self.assertEqual(self.starting_values.get_startY(), 60)
@@ -29,12 +29,12 @@ class TestColorIntegration(unittest.TestCase):
         self.assertEqual(self.starting_values.get_state(), "Start")
         self.assertEqual(self.starting_values.get_height(), 20)
         self.assertEqual(self.starting_values.get_width(), 10)
-
+    #Regression
     def test_set_state_method(self):
         self.assertEqual(self.starting_values.get_state(), "Start")
         self.starting_values.set_state("NewState")
         self.assertEqual(self.starting_values.get_state(), "NewState")
-
+    
     
 
     
