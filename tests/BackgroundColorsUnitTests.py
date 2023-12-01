b68657015def95b782afbacd1f044d9a140c3216
juliannatruitt <143114521+juliannatruitt@users.noreply.github.com>
2023-12-01 14:53:20 -0500

    Update BackgroundColorsUnitTests.py

--- tests/BackgroundColorsUnitTests.py
+++ tests/BackgroundColorsUnitTests.py
@@ -1,5 +1,8 @@
 import unittest
 import pygame
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import final_tetris_refactored as ftr
 
 class TestBackgroundColors(unittest.TestCase):
